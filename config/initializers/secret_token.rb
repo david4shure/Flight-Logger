a6f991a6a5f5438ea5adda32520d391d7f050008
David Shure <david.shure@elementaltechnologies.com>
2015-01-01 15:14:50 -0800

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlightLogger::Application.config.secret_token = 'fa28422015cd7d3746a12ad160a583a1ec04334279e341e9440494c0bc3bec88e0e8a61f368985491dc6fe612b88cb4d4f262c9cecb8724279b21b82ff93c64a'
