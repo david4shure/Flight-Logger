b38b0c26fe261dcba1f190cb1fc4afbcc73ab726
David Shure <david.shure@elementaltechnologies.com>
2015-01-11 02:07:28 -0800

    Fixing session issue with safari

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-FlightLogger::Application.config.session_store :cookie_store, key: '_flight-logger_session'
+FlightLogger::Application.config.session_store :cookie_store, {
+  key: '_flight-logger_session',
+  :expire_after => 60*24*60*60
+}
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
