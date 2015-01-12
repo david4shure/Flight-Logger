bab2fe8a5e61c393f05fec2724270a392da6f498
johnny4shure <dshure@westmont.edu>
2015-01-11 21:28:04 -0800

    Enabling CORS. Sorry security, you're screwed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :enable_cors
+
+  def enable_cors
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
 
   protected
   def authenticate_user!
