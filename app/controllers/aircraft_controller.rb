20762eed9c998449b7b32c5af358b425277ae981
johnny4shure <dshure@westmont.edu>
2015-01-11 21:50:02 -0800

    Mocking aircraft views, they suck now but they will improve

--- app/controllers/aircraft_controller.rb
+++ app/controllers/aircraft_controller.rb
@@ -1,8 +1,15 @@
 class AircraftController < ApplicationController
   def new
+    @aircraft = Aircraft.new
   end
 
   def create
+    @aircraft = current_user.aircraft.create params[:aircraft]
+    if @aircraft.save
+      redirect_to aircraft_path(@aircraft)
+    else
+      render "/aircraft/new"
+    end
   end
   
   def show
